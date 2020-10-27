87504e662e538026f25f8d150151b4ce67553140
Anett <holcza@gmail.com>
2020-10-27 23:21:15 +0100

    Registration solution

---
+++ src/main/java/solutions/classstructureio/Registration.java
@@ -0,0 +1,15 @@
+package solutions.classstructureio;
+
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Add username");
+        String username= scanner.nextLine();
+        System.out.println("Add email");
+        String email= scanner.nextLine();
+        System.out.println("You registered with username: "+username+" and email: "+email);
+
+    }
+}
