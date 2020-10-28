8d3f0519d5536544213b9d8f3cb77eff37493eac
Anett <holcza@gmail.com>
2020-10-28 22:25:28 +0100

    BankAccount class with UML

---
+++ src/main/java/solutions/classstructureintegrate/BankAccount.java
@@ -0,0 +1,29 @@
+package solutions.classstructureintegrate;
+
+public class BankAccount {
+    private String accountNumber;
+    private String owner;
+    private int balance;
+
+    public BankAccount(String accountNumber, String owner, int balance) {
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(int amount){
+        this.balance=this.balance+amount;
+    }
+    public void withdraw(int amount){
+        this.balance=this.balance-amount;
+    }
+
+    public void transfer(BankAccount bankAccount,int amount){
+        this.balance=this.balance-amount;
+        bankAccount.deposit(amount);
+    }
+
+    public String getInfo(){
+        return owner+" ("+accountNumber+"): "+balance+" Ft";
+    }
+}
