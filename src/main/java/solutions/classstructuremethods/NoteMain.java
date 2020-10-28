8a3eb0723a0ca36773b41a0860816770f923e89c
Anett <holcza@gmail.com>
2020-10-28 21:13:14 +0100

    Note class for methods

---
+++ src/main/java/solutions/classstructuremethods/NoteMain.java
@@ -0,0 +1,11 @@
+package solutions.classstructuremethods;
+
+public class NoteMain {
+    public static void main(String[] args) {
+        Note note=new Note();
+        note.setName("Ági");
+        note.setTopic("bevásárlás");
+        note.setText("tojás, alma, kenyér, tej");
+        note.getNoteText();
+    }
+}
