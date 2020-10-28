13a2686d7828f61b530c1632c04797fdd4e83be3
Anett <holcza@gmail.com>
2020-10-28 21:43:06 +0100

    Store class for constructors

---
+++ src/main/java/solutions/classstructureconstructors/StoreMain.java
@@ -0,0 +1,12 @@
+package solutions.classstructureconstructors;
+
+public class StoreMain {
+    public static void main(String[] args) {
+        Store store1 =new Store("szék");
+        Store store2 =new Store("asztal");
+        store1.store(3);
+        store2.dispatch(5);
+        System.out.println(store1.getProduct()+" : "+store1.getStock()+" , "+store2.getProduct()+" : "+store2.getStock());
+
+    }
+}
