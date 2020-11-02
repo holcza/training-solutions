043aba4a714e09e720da280f6658c6e505373438
Anett <holcza@gmail.com>
2020-11-02 20:18:00 +0100

    Registration

---
+++ src/main/java/solutions/stringtype/registration/UserValidator.java
@@ -0,0 +1,20 @@
+package solutions.stringtype.registration;
+
+public class UserValidator {
+
+    public boolean isValidUsername (String username){
+        boolean isValidUsername = username=="" ? false : true;
+        return isValidUsername;
+    }
+
+    public boolean isValidPassword (String password1, String password2){
+        boolean isValidPassword = password1.length()<8 || password2.length()<8 || !password1.equals(password2)? false:true;
+        return isValidPassword;
+
+    }
+
+    public boolean isValidEmail(String email){
+        boolean isValidEmail = email.indexOf("@")==-1 || email.indexOf(".")==-1 ||email.indexOf("@")==0 || email.indexOf(".")==email.length()-1||email.indexOf("@")+1==email.indexOf(".") ? false : true;
+        return isValidEmail;
+    }
+}
