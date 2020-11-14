745d6ee68a2f557a85bf54e1546c51ae47cad629
Anett <holcza@gmail.com>
2020-11-14 15:53:08 +0100

    Code Formatting

--- src/main/java/match/Point.java
+++ src/main/java/match/Point.java
@@ -5,4 +5,21 @@ public class Point {
     private int x;
     private int y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance (Point other){
+
+        return Math.max(Math.abs(other.getX()-x),Math.abs(other.getY()-y));
+    }
 }
