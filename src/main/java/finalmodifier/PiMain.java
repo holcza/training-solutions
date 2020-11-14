e1f6b04ba9a0a4f24490e2a4bb6221f6a6d3a668
Anett <holcza@gmail.com>
2020-11-14 12:55:24 +0100

    CylinderCalculator and CircleCalculator

--- src/main/java/finalmodifier/PiMain.java
+++ src/main/java/finalmodifier/PiMain.java
@@ -5,10 +5,13 @@ public class PiMain {
         CylinderCalculator cylinderCalculator = new CylinderCalculator();
         double r = 2;
         double h = 3;
+        final double z;
         System.out.println("A PI értéke: "+CircleCalculator.PI);
         System.out.println("A henger felülete: "+cylinderCalculator.calculateSurfaceArea(r,h));
         System.out.println("A henger térfogata: "+cylinderCalculator.calculateVolume(r,h));
 
+        CircleCalculator circleCalculator = new CircleCalculator();
+        System.out.println(circleCalculator.calculatePerimeter(10));
 
     }
 }
