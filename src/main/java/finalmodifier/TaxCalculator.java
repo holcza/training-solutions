0466567bf6df820a7b0df7a438c73def5abf643a
Anett <holcza@gmail.com>
2020-11-14 12:50:02 +0100

    TaxCalculator

---
+++ src/main/java/finalmodifier/TaxCalculator.java
@@ -0,0 +1,21 @@
+package finalmodifier;
+
+public class TaxCalculator {
+
+    public final static double TAX = 0.27;
+
+    public double tax (double price){
+        return price*TAX;
+    }
+
+    public double priceWithTax (double price){
+
+        return price*TAX + price;
+    }
+
+    public static void main(String[] args) {
+        TaxCalculator taxCalculator = new TaxCalculator();
+        System.out.println(taxCalculator.tax(100));
+        System.out.println(taxCalculator.priceWithTax(100));
+    }
+}
