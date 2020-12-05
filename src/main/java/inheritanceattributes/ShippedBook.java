f617fb1b4e75edc09988b2e52c86171229ce5ec9
Anett <holcza@gmail.com>
2020-12-05 18:36:29 +0100

    ShippedBook

---
+++ src/main/java/inheritanceattributes/ShippedBook.java
@@ -0,0 +1,20 @@
+package inheritanceattributes;
+
+public class ShippedBook extends Book {
+
+    private int shippingCost;
+
+    public ShippedBook(String title, int price, int shippingCost) {
+        super(title, price);
+        this.shippingCost = shippingCost;
+    }
+
+    public int order(int pieces) {
+        return super.purchase(pieces) + shippingCost;
+    }
+
+    @Override
+    public String toString() {
+        return getTitle() +":"+ price + " - " + shippingCost;
+    }
+}
