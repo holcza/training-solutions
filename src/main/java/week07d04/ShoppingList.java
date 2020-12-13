881b33c8cd93e2947ea09725a1b440531518d56e
Anett <holcza@gmail.com>
2020-12-13 15:34:49 +0100

    ShoppingList

--- src/main/java/week07d04/ShoppingList.java
+++ src/main/java/week07d04/ShoppingList.java
@@ -2,26 +2,35 @@ package week07d04;
 
 import week06d04.ShoppingCart;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class ShoppingList {
 
     public long calculatePath(String path) {
-        Scanner scanner = new Scanner(path);
         String pathLine = "";
         long sum = 0;
-        while (scanner.hasNextLine()) {
-            pathLine = scanner.nextLine();
-            String[] parts = pathLine.split(";");
-            sum += Long.parseLong(parts[1]) * Long.parseLong(parts[2]);
-        }
+        try(Scanner scanner = new Scanner(Path.of(path), StandardCharsets.UTF_8)){
 
+            while (scanner.hasNextLine()) {
+                pathLine = scanner.nextLine();
+                String[] parts = pathLine.split(";");
+                sum += Long.parseLong(parts[1]) * Long.parseLong(parts[2]);
+            }
+
+
+        } catch (IOException ioe){
+            throw new IllegalStateException("File can not be read",ioe);
+        }
         return sum;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         ShoppingList shoppingList = new ShoppingList();
-        long sum =  shoppingList.calculatePath("kenyér;2;3\ntej;3;4");
+        System.out.println(Path.of("src/main/resources/week07d04.shoppingList.dat").toRealPath());
+        long sum =  shoppingList.calculatePath("src/main/resources/week07d04.shoppingList.dat");
         System.out.println(sum);
     }
 }
