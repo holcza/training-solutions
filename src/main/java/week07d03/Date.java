4a6062091017888ccc0b26a869942e180332ea39
Anett <holcza@gmail.com>
2020-12-13 13:39:16 +0100

    Date

--- src/main/java/week07d03/Date.java
+++ src/main/java/week07d03/Date.java
@@ -8,7 +8,7 @@ public class Date {
     private final int month;
     private final int day;
 
-    public Date(int year, int month, int day) {
+    private Date(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
