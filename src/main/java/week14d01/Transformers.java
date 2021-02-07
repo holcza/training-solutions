54ecf2d50f6d538ba6e6876e15e40bc83534f97e
Anett <holcza@gmail.com>
2021-02-07 16:33:32 +0100

    Transformers

---
+++ src/main/java/week14d01/Transformers.java
@@ -0,0 +1,24 @@
+package week14d01;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Transformers {
+    public <T, R> List<R> map(List<T> list, Function<T, R> transformer) {
+        return list.stream().map(transformer).collect(Collectors.toList());
+    }
+
+    public <T> T reduce(List<T> list, T identity, BiFunction<T, T, T> combiner) {
+        return list.stream().reduce(identity, combiner::apply);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Transformers().map(Arrays.asList(1, 2, 3), (value) -> value + 1));
+// [2, 3, 4]
+        System.out.println(new Transformers().reduce(Arrays.asList(1, 2, 3), 0, Integer::sum));
+// 6
+    }
+}
