2e0ab13e840df30c7d1a96238f3f9c29fa6066b0
Anett <holcza@gmail.com>
2021-02-13 15:09:12 +0100

    CharacterCounter

--- src/main/java/week15d04/CharType.java
+++ src/main/java/week15d04/CharType.java
@@ -1,4 +1,23 @@
 package week15d04;
 
 public enum CharType {
+    CONSONANT ("aáeéiíoóöőuúüű"), VOWEL ("bcdfghjklmnpqrstvwxyz"), OTHER("");
+
+    private String chars;
+
+    CharType(String chars) {
+        this.chars = chars;
+    }
+
+    public static CharType getType (int c){
+        if (CONSONANT.chars.contains(String.valueOf((char)c))){
+            return CONSONANT;
+        } else if (VOWEL.chars.contains(String.valueOf((char)c))){
+            return VOWEL;
+        } else {
+            return OTHER;
+        }
+    }
+
+
 }
--- src/main/java/week15d04/CharacterCounter.java
+++ src/main/java/week15d04/CharacterCounter.java
@@ -1,4 +1,33 @@
 package week15d04;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CharacterCounter {
+
+    private static boolean bySpace(int x) {
+        return x != (char) ' ';
+    }
+
+    public Map<CharType,Long> count (Path path){
+        try(Stream<String > s = Files.lines(path)){
+            return s.map(String::toLowerCase)
+                    .flatMapToInt(String::chars)
+                    .filter(CharacterCounter::bySpace)
+                    .mapToObj(CharType::getType)
+                    .collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+        }catch (IOException ioe){
+            throw new IllegalStateException("Can not read file",ioe);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new CharacterCounter().count(Path.of("test.txt")));
+    }
 }
