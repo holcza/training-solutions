63181cb9233ec6b0d1efd8ab7d6780b4c1bc87e1
Anett <holcza@gmail.com>
2021-02-15 19:33:01 +0100

    exam03_Histogram

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -3,25 +3,25 @@ package exam03;
 import java.io.*;
 
 public class Histogram {
-    public String createHistogram (BufferedReader reader) throws IOException{
-        String result ="";
+    public String createHistogram(BufferedReader reader) throws IOException {
+        String result = "";
 
-            String line;
-            while ((line = reader.readLine())!=null){
+        String line;
+        while ((line = reader.readLine()) != null) {
 
-                result = processLine(result, line);
-            }
+            result = processLine(result, line);
+        }
 
 
         return result;
     }
 
     private String processLine(String result, String line) {
-        int numberOfStars =Integer.parseInt(line);
-        for(int i = 0; i<numberOfStars;i++){
-            result +="*";
+        int numberOfStars = Integer.parseInt(line);
+        for (int i = 0; i < numberOfStars; i++) {
+            result += "*";
         }
-        result +="\n";
+        result += "\n";
         return result;
     }
 
