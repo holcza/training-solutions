9db95c1cbf5e68eab99a17b138fb4c3e30171d37
Anett <holcza@gmail.com>
2021-02-15 19:25:55 +0100

    exam03_Histogram

---
+++ src/main/java/exam03/Histogram.java
@@ -0,0 +1,29 @@
+package exam03;
+
+import java.io.*;
+
+public class Histogram {
+    public String createHistogram (BufferedReader reader) throws IOException{
+        String result ="";
+
+            String line;
+            while ((line = reader.readLine())!=null){
+
+                result = processLine(result, line);
+            }
+
+
+        return result;
+    }
+
+    private String processLine(String result, String line) {
+        int numberOfStars =Integer.parseInt(line);
+        for(int i = 0; i<numberOfStars;i++){
+            result +="*";
+        }
+        result +="\n";
+        return result;
+    }
+
+
+}
