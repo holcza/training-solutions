c32cebbac877db92df93aca1385523c856320486
Anett <holcza@gmail.com>
2021-03-07 17:47:52 +0100

    activities DB

--- src/main/java/activityTracker/ActivityDao.java
+++ src/main/java/activityTracker/ActivityDao.java
@@ -14,22 +14,38 @@ public class ActivityDao {
         this.dataSource = dataSource;
     }
 
-    public void saveActivity(Activity activity){
+    public Activity saveActivity(Activity activity) {
         try (
                 Connection conn = dataSource.getConnection();
                 PreparedStatement stmt =
-                        conn.prepareStatement("insert into activities(start_time,activity_des,activity_type) values (?,?,?)")) {
+                        conn.prepareStatement("insert into activities(start_time,activity_des,activity_type) values (?,?,?);",
+                                Statement.RETURN_GENERATED_KEYS)) {
             stmt.setString(1, String.valueOf(Timestamp.valueOf(activity.getStartTime())));
             stmt.setString(2, activity.getDesc());
             stmt.setString(3, activity.getType().toString());
             stmt.executeUpdate();
-        }
-        catch (SQLException se) {
+            return executeAndGetGeneratedKey(stmt, activity);
+        } catch (SQLException se) {
             throw new IllegalStateException("Cannot insert", se);
         }
     }
 
-    public Activity findActivityById(long id){
+    private Activity executeAndGetGeneratedKey(PreparedStatement stmt, Activity activity) {
+        try (
+                ResultSet rs = stmt.getGeneratedKeys();
+        ) {
+            if (rs.next()) {
+                long id = rs.getLong(1);
+                return new Activity((int) id, activity.getStartTime(), activity.getDesc(), activity.getType());
+            } else {
+                throw new SQLException("No key has generated");
+            }
+        } catch (SQLException sqle) {
+            throw new IllegalArgumentException("Error by insert", sqle);
+        }
+    }
+
+    public Activity findActivityById(long id) {
         try (
                 Connection conn = dataSource.getConnection();
                 PreparedStatement stmt =
@@ -52,7 +68,7 @@ public class ActivityDao {
                 LocalDateTime localDateTime = rs.getTimestamp("start_time").toLocalDateTime();
                 String desc = rs.getString("activity_des");
                 String type = rs.getString("activity_type");
-                return new Activity(id,localDateTime,desc,ActivityType.valueOf(type));
+                return new Activity(id, localDateTime, desc, ActivityType.valueOf(type));
             }
             throw new IllegalArgumentException("No result");
         } catch (SQLException sqle) {
@@ -60,7 +76,7 @@ public class ActivityDao {
         }
     }
 
-    public List<Activity> listActivities(){
+    public List<Activity> listActivities() {
         try (
                 Connection conn = dataSource.getConnection();
                 Statement stmt = conn.createStatement();
@@ -72,11 +88,10 @@ public class ActivityDao {
                 LocalDateTime localDateTime = rs.getTimestamp("start_time").toLocalDateTime();
                 String desc = rs.getString("activity_des");
                 String type = rs.getString("activity_type");
-                activities.add(new Activity(id,localDateTime,desc,ActivityType.valueOf(type)));
+                activities.add(new Activity(id, localDateTime, desc, ActivityType.valueOf(type)));
             }
             return activities;
-        }
-        catch (SQLException se) {
+        } catch (SQLException se) {
             throw new IllegalStateException("Cannot select activities", se);
         }
     }
