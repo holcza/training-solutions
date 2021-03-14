6f589babbb9960bb13324fd7eafe10a2e1d6c41d
Anett <holcza@gmail.com>
2021-03-14 22:20:31 +0100

    Covid vaccination project

--- src/main/java/covidproject/ui/MenuServiceMain.java
+++ src/main/java/covidproject/ui/MenuServiceMain.java
@@ -103,28 +103,38 @@ public class MenuServiceMain {
         int citizenID = vaccinationStatus.getCitizen_id();
         int numberOfVaccination = vaccinationStatus.getNumberOfVaccination();
         LocalDate lastDate = vaccinationStatus.getLastDate();
-        if (numberOfVaccination==2){
-            throw new IllegalStateException("Already 2 vaccinations were given");
+        switch (numberOfVaccination) {
+            case 2:
+                throw new IllegalStateException("Already 2 vaccinations were given");
+
+            case 1:
+                saveSecondVaccinationGiven(citizenID, numberOfVaccination, lastDate);
+                break;
+            case 0:
+                saveFirstVaccinationGiven(citizenID, numberOfVaccination);
+                break;
         }
-        if (numberOfVaccination==1){
-            if (Period.between(lastDate,LocalDate.now()).getDays()<15){
-                throw new IllegalStateException("Last vaccination was less than 15 days before, on day: "+lastDate);
-            } else {
-                System.out.println("Last vaccination was on day: "+ lastDate);
-                Brand brand = vaccinationsDao.getBrandOfPreviousVaccination(citizenID);
-                System.out.println("Brand of last vaccination: "+ brand);
-                LocalDate date = getNewLastDate();
-                vaccinationsDao.insertVaccination(new Vaccination(citizenID, date, Status.SECOND,brand));
-                updateVaccinationStatus(citizenID, numberOfVaccination+1,date);
-            }
-        }
-        if (vaccinationStatus.getNumberOfVaccination()==0){
-            System.out.println("No vaccination was given yet");
-            System.out.println("Please give the vaccine brand which was given today: ");
-            Brand brand = Brand.valueOf(scanner.nextLine());
+    }
+
+    private void saveFirstVaccinationGiven(int citizenID, int numberOfVaccination) {
+        System.out.println("No vaccination was given yet");
+        System.out.println("Please give the vaccine brand which was given today: ");
+        Brand brand = Brand.valueOf(scanner.nextLine());
+        LocalDate date = getNewLastDate();
+        vaccinationsDao.insertVaccination(new Vaccination(citizenID, date, Status.FIRST, brand));
+        updateVaccinationStatus(citizenID, numberOfVaccination + 1, date);
+    }
+
+    private void saveSecondVaccinationGiven(int citizenID, int numberOfVaccination, LocalDate lastDate) {
+        if (Period.between(lastDate, LocalDate.now()).getDays() < 15) {
+            throw new IllegalStateException("Last vaccination was less than 15 days before, on day: " + lastDate);
+        } else {
+            System.out.println("Last vaccination was on day: " + lastDate);
+            Brand brand = vaccinationsDao.getBrandOfPreviousVaccination(citizenID);
+            System.out.println("Brand of last vaccination: " + brand);
             LocalDate date = getNewLastDate();
-            vaccinationsDao.insertVaccination(new Vaccination(citizenID,date,Status.FIRST,brand));
-            updateVaccinationStatus(citizenID, numberOfVaccination+1,date);
+            vaccinationsDao.insertVaccination(new Vaccination(citizenID, date, Status.SECOND, brand));
+            updateVaccinationStatus(citizenID, numberOfVaccination + 1, date);
         }
     }
 
@@ -167,6 +177,10 @@ public class MenuServiceMain {
 
         List<Person> citizens = citizensDao.getCitizensByZip(zip);
 
+        saveListOfCitizensToVaccinate(zip, file);
+    }
+
+    private void saveListOfCitizensToVaccinate(String zip, String file) {
         try (BufferedWriter writer = Files.newBufferedWriter(Path.of(file))) {
             FileHandler.writeLines(zip, writer, dataSource);
         } catch (IOException ioe) {
@@ -175,12 +189,26 @@ public class MenuServiceMain {
     }
 
     private void registrationWithFile() {
+        String path = checkPath();
+        List<Person> citizens = readRegistrationFile(path);
+
+        citizensDao.insertCitizen(citizens);
+
+        System.out.println("Registration completed");
+    }
+
+    private String checkPath() {
         System.out.println("Please give the filepath: ");
         List<Person> citizens;
         String path = scanner.nextLine();
         if (!Files.exists(Path.of(path))) {
             throw new IllegalArgumentException("Path given does not exists");
         }
+        return path;
+    }
+
+    private List<Person> readRegistrationFile(String path) {
+        List<Person> citizens;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(path));
             citizens = FileHandler.readLines(reader, dataSource);
@@ -188,28 +216,54 @@ public class MenuServiceMain {
         } catch (IOException ioe) {
             throw new IllegalStateException("Can not read file", ioe);
         }
+        return citizens;
+    }
+
+    private void registrationByPerson() {
+        String name = getNameInput();
+
+        String zip = getZipInput();
+
+        int age = getAgeInput();
+
+        String email = getEmailInput();
+
+        String taj = getTajInput();
+
+        List<Person> citizens = new ArrayList<>();
+        citizens.add(new Person(name, zip, age, email, taj));
 
         citizensDao.insertCitizen(citizens);
 
         System.out.println("Registration completed");
+
     }
 
-    private void registrationByPerson() {
+    private String getNameInput(){
         System.out.println("Please give me your full name: ");
         String name = scanner.nextLine();
         PersonValidator.emptyCheck(name);
+        return name;
+    }
 
+    private String getZipInput(){
         System.out.println("Please give me your zip number: ");
         String zip = scanner.nextLine();
         PersonValidator.emptyCheck(zip);
         PersonValidator.zipCheck(zip, dataSource);
         System.out.println("Your city is: " + citiesDao.listCityNames(zip));
+        return zip;
+    }
 
+    private int getAgeInput(){
         System.out.println("Please give me your age: ");
         int age = scanner.nextInt();
         scanner.nextLine();
         PersonValidator.ageCheck(age);
+        return age;
+    }
 
+    private String getEmailInput(){
         System.out.println("Please give me your email: ");
         String email1 = scanner.nextLine();
         PersonValidator.emailCheck(email1);
@@ -217,18 +271,14 @@ public class MenuServiceMain {
         System.out.println("Please give me your email again for validation: ");
         String email2 = scanner.nextLine();
         PersonValidator.sameEmailCheck(email1, email2);
+        return email1;
+    }
 
+    private String getTajInput(){
         System.out.println("Please give me your taj number: ");
         String taj = scanner.nextLine();
         PersonValidator.tajCheck(taj);
-
-        List<Person> citizens = new ArrayList<>();
-        citizens.add(new Person(name, zip, age, email1, taj));
-
-        citizensDao.insertCitizen(citizens);
-
-        System.out.println("Registration completed");
-
+        return taj;
     }
 
     public static void main(String[] args) {
